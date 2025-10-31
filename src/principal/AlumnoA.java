32366a2ea37df54f840871ce0ece8f213d17e57a
Izam Molano <izammoladi@gmail.com>
2025-10-31 12:33:00 +0100

    prueba

---
+++ src/principal/AlumnoA.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class AlumnoA {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Esto es una prueba del alumnoA");
+	}
+
+}
