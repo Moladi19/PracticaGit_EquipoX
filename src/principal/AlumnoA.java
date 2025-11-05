2a64abd72400583211f5afad39a14e1a45ad64bf
Izam Molano <izammoladi@gmail.com>
2025-11-05 13:02:29 +0100

    t54t

--- src/principal/AlumnoA.java
+++ src/principal/AlumnoA.java
@@ -4,7 +4,7 @@ public class AlumnoA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Esto es una prueba del alumnoA");
+		System.out.println("Esto es una prueba del alumnoA");
 	}
 
 }
